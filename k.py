5a719551e9629451819f8af636f7195d4a2268fe
Priyankaendluri <endlurupriyanka@gmail.com>
2025-03-03 15:38:22 +0530

    Add files via upload

---
+++ k.py
@@ -0,0 +1,11 @@
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+c=int(input("Enter a number"))
+if(a>b)and(a>c):      
+        print(a )
+elif(b>a)and(b>c):
+        print(b)
+elif(c>a)and(c>b):
+        print(c) 
+else:
+    print("error")
